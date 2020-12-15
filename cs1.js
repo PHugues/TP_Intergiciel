4b242936499010de8105f8a013615342a4a269b9
Pierre <pierre.hugues1998@gmail.com>
2020-12-15 16:28:13 +0100

    Bdd

--- cs1.js
+++ cs1.js
@@ -29,17 +29,17 @@ async function getTopicData() {
                     // Check if world table isn't empty
                     let world = await request.GetRow("Select * From world");
                     if(!world) {
-                        await request.ExecSql('Insert into world Values($1, $2, $3, $4, $5, $6, current_date)', [Global.NewConfirmed, Global.TotalConfirmed, Global.NewDeaths, Global.TotalDeaths, Global.NewRecovered, Global.TotalRecovered]);
+                        await request.ExecSql('Insert into world Values($1, $2, $3, $4, $5, $6, current_timestamp)', [Global.NewConfirmed, Global.TotalConfirmed, Global.NewDeaths, Global.TotalDeaths, Global.NewRecovered, Global.TotalRecovered]);
                     } else {
-                        await request.ExecSql('Update world Set "NewConfirmed" = $1, "TotalConfirmed" = $2, "NewDeaths" = $3, "TotalDeaths" = $4, "NewRecovered" = $5, "TotalRecovered" = $6, "Datemaj" = current_date', [Global.NewConfirmed, Global.TotalConfirmed, Global.NewDeaths, Global.TotalDeaths, Global.NewRecovered, Global.TotalRecovered]);
+                        await request.ExecSql('Update world Set "newconfirmed" = $1, "totalconfirmed" = $2, "newdeaths" = $3, "totaldeaths" = $4, "newrecovered" = $5, "totalrecovered" = $6, "datemaj" = current_timestamp', [Global.NewConfirmed, Global.TotalConfirmed, Global.NewDeaths, Global.TotalDeaths, Global.NewRecovered, Global.TotalRecovered]);
                     }
                     for(let country of Countries) {
                         // Check if country tuple already exists
-                        let c = await request.GetRow('Select * From countries Where "CountryCode"=$1', [country.CountryCode]);
+                        let c = await request.GetRow('Select * From countries Where "countrycode"=$1', [country.CountryCode]);
                         if(!c) {
-                            await request.ExecSql('Insert into countries Values($1, $2, $3, $4, $5, $6, $7, $8, $9, current_date)', [country.Country, country.CountryCode, country.Slug, country.NewConfirmed, country.TotalConfirmed, country.NewDeaths, country.TotalDeaths, country.NewRecovered, country.TotalRecovered]);
+                            await request.ExecSql('Insert into countries Values($1, $2, $3, $4, $5, $6, $7, $8, $9, current_timestamp)', [country.Country, country.CountryCode, country.Slug, country.NewConfirmed, country.TotalConfirmed, country.NewDeaths, country.TotalDeaths, country.NewRecovered, country.TotalRecovered]);
                         } else {
-                            await request.ExecSql('Update countries Set "NewConfirmed" = $1, "TotalConfirmed" = $2, "NewDeaths" = $3, "TotalDeaths" = $4, "NewRecovered" = $5, "TotalRecovered" = $6, "Datemaj" = current_date Where "CountryCode" = $7', [country.NewConfirmed, country.TotalConfirmed, country.NewDeaths, country.TotalDeaths, country.NewRecovered, country.TotalRecovered, country.CountryCode]);
+                            await request.ExecSql('Update countries Set "newconfirmed" = $1, "totalconfirmed" = $2, "newdeaths" = $3, "totaldeaths" = $4, "newrecovered" = $5, "totalrecovered" = $6, "datemaj" = current_timestamp Where "countrycode" = $7', [country.NewConfirmed, country.TotalConfirmed, country.NewDeaths, country.TotalDeaths, country.NewRecovered, country.TotalRecovered, country.CountryCode]);
                         }
                     }
                     console.info("Updated countries");
